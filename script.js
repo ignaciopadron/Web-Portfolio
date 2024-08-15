565b8574ba15a48c6587505fe7dbbc01a7b13b73
Ignacio Padrón <ignaciopadrond@gmail.com>
2024-08-15 19:01:31 +0200

    Tercer commit - version 3 JavaScript más limpio y eficiente

--- script.js
+++ script.js
@@ -1,46 +1,37 @@
 let menuVisible = false;
-//Función que oculta o muestra el menu
-function mostrarOcultarMenu(){
+
+//Función que oculta o muestra el menu -------------------------------------------------------------------
+
+function mostrarOcultarMenu() {
     const nav = document.getElementById("nav");
-    if (menuVisible){
-        nav.classList.remove("responsive");
-        menuVisible = false;
-    }else {
-        nav.classList.add("responsive");
-        menuVisible = true;
-    }
+    nav.classList.toggle("responsive");
+    menuVisible = !menuVisible;
 }
 
-// Oculta el menú cuando se hace clic fuera de él
+// Oculta el menú cuando se hace clic fuera de él ------------------------------------------------------
+
 document.addEventListener('click', function(event) {
     const nav = document.getElementById("nav");
     const menuButton = document.querySelector(".nav-responsive");
     if (menuVisible && !nav.contains(event.target) && !menuButton.contains(event.target)) {
-        nav.classList.remove("responsive");
-        menuVisible = false;
+        mostrarOcultarMenu();
     }
 });
 
-//Oculta el menú cuando el ratón no está sobre él (solo en pantallas móviles si se considera necesario)
+//Oculta el menú cuando el ratón no está sobre él (solo en pantallas móviles si se considera necesario) -------
+
 document.getElementById("nav").addEventListener('mouseleave', function() {
     if (menuVisible) {
-        document.getElementById("nav").classList.remove("responsive");
-        menuVisible = false;
+        mostrarOcultarMenu();
     }
 });
 
-
-
-
-
-
-function seleccionar(){
-    //oculto el menu una vez que selecciono una opcion
-    document.getElementById("nav").classList ="";
-    menuVisible = false;
+function seleccionar() {
+    mostrarOcultarMenu();
 }
 
-// Aparece la sección sobre mi
+// Aparece la sección sobre mi ------------------------------------------------------------------------
+
 document.addEventListener('DOMContentLoaded', () => {
     const aboutMeSection = document.querySelector('.sobremi');
 
@@ -56,9 +47,8 @@ document.addEventListener('DOMContentLoaded', () => {
     observer.observe(aboutMeSection);
 });
 
+ // Datos del gráfico de radar ---------------------------------------------------------------------------
 
-
- // Datos del gráfico de radar
  const data = {
     labels: ['' ,'HTML & CSS', 'JavaScript', 'Python', 'database', 'Web server', 'Virtual Machine', 'VPN', 'SAMBA', 'Networks', 'OS'],
     datasets: [{
@@ -75,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }]
 };
 
-// Opciones del gráfico de radar
+// Opciones del gráfico de radar --------------------------------------------------------------------------------
 const options = {
     scales: {
         r: { // 'r' para el eje radial en Chart.js 3.x y posteriores
@@ -103,7 +93,8 @@ const options = {
     }
 };
 
-// Crear el gráfico de radar
+// Crear el gráfico de radar -------------------------------------------------------------------------
+
 const ctx = document.getElementById('technicalskills').getContext('2d');
 new Chart(ctx, {
     type: 'radar',
@@ -118,6 +109,7 @@ const printCharts = () => {
 
 const renderModelsChart = () => {
 
+    // Datos del gráfico de donut ---------------------------------------------------------------------------
 
     const data = {
         labels: ['Comunicación', 'Trabajo en Equipo', 'Resolución de problemas', 'Dedicación y constancia', 'Proyect Management', 'Productividad', 'Trabajar bajo presión', 'Proactividad y Aprendizaje', 'Documentación', 'Adaptabilidad'],
@@ -151,11 +143,9 @@ const renderModelsChart = () => {
             ],
             borderWidth: 4,
             hoverOffset: 20,
-            
-            
-       
         }]
     }   
+    // Opciones del gráfico de donut --------------------------------------------------------------------------
 
     const options = {
         
@@ -171,17 +161,16 @@ const renderModelsChart = () => {
 
 }
 
-
 printCharts()
 
-//detecto el scrolling para aplicar la animacion de la barra de habilildades
-window.onscroll = function(){
-    efectoHabilidades();
-}
+//detecto el scrolling para aplicar la animacion de la barra de habilildades ----------------------------------
 
-// Ejemplo simple de cómo podrías agregar interactividad
 document.getElementById("skill-octagon").addEventListener("mouseover", function(event) {
-    // Cambiar el color de la sección cuando el ratón está sobre ella
     event.target.style.fill = "lightblue";
 });
+
+document.getElementById("skill-octagon").addEventListener("mouseout", function(event) {
+    event.target.style.fill = "";
+});
+
 
